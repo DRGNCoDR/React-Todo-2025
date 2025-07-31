dbbc92ccc2df0da6d3bde40973c98fa298c381f0
DRGNCoDR <rayfradella@yahoo.com>
2025-07-31 02:04:21 -0400

    Update todos.js

--- src/todos.js
+++ src/todos.js
@@ -3,33 +3,21 @@ import Add from "./comp/add"
 import List from "./comp/list"
 
 function Todos() {
-    const border1 = {
-        border : "1px solid black"
-    }
-    const border2 = {
-        border : "2px dotted black"
+    const container = {
+        border : "1px solid black",
+        margin : "2px"
     }
 
     const pad5 = {
-        padding : "5px"
-    }
-
-    const margin5 = {
-        margin : "5px"
+        padding : "2px"
     }
 
     const [todos, setTodo] = useState([])
 
     return (
-        <div style = {border1}>
-            <div
-                style = {
-                    {
-                        ...border1,
-                        ...pad5,
-                        ...margin5
-                    }
-                }
+        <div style = {container}>
+            <div 
+                style = {{...container, ...pad5}}
             >
                 <Add
                     todos = {todos}
@@ -37,18 +25,10 @@ function Todos() {
                 />
             </div>
 
-            <div
-                style = {
-                    {
-                        ...border2,
-                        ...pad5,
-                        ...margin5
-                    }
-                }
+            <div 
+                style = {{...container, ...pad5}}
             >
-                <List
-                    todos = {todos}
-                />
+                <List todos = {todos}/>
             </div>
         </div>
     )
